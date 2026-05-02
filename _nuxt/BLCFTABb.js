9b743f77b3daf909246d42ec13daee48d0f04373
wumacms <wumacms@users.noreply.github.com>
2026-05-02 04:52:53 +0000

    deploy: fe9f9c51f320755cef1820b2258bef3a81aac0de

--- _nuxt/BLCFTABb.js
+++
@@ -1,1 +0,0 @@
-import{m as n}from"./9Fhrlut4.js";function c(){const e=n().app.baseURL||"";return{cdnBaseUrl:e,getImageUrl:r=>{if(!r)return"";if(r.startsWith("http"))return r;const t=r.startsWith("/")?r:`/${r}`;return`${e.replace(/\/$/,"")}${t}`}}}export{c as u};
